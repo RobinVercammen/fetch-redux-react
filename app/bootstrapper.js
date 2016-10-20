b97dc1c51463940fbe5d7158ea6930e757bd1776
Robin Vercammen <robinvercammen1993@hotmail.com>
2016-10-20 19:51:53 +0200

    load button added

--- app/bootstrapper.js
+++ app/bootstrapper.js
@@ -1,9 +1,13 @@
 import GetDashboard from './api/DashboardApi';
 
 const applicationBootstrapper = () => {
-    GetDashboard(1).then(data => {
-        document.getElementById('applicatie').innerText = JSON.stringify(data);
-    });
+    window.onload = () => {
+        document.getElementById('load-button').onclick = (ev) => {
+            GetDashboard(1).then(data => {
+                document.getElementById('applicatie').innerText = JSON.stringify(data);
+            });
+        }
+    }
 }
 
 export default applicationBootstrapper;
