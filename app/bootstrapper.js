1b1deb83617358e06959bcd28d686a74be3236db
Robin Vercammen <robinvercammen1993@hotmail.com>
2016-10-21 17:08:17 +0200

    weight component

--- app/bootstrapper.js
+++ app/bootstrapper.js
@@ -9,27 +9,10 @@ renderApplication();
 
 const applicationBootstrapper = () => {
     window.onload = () => {
-        Store.subscribe(() => {
-            document.getElementById('applicatie-1').innerHTML = `
-            <pre>
-                ${JSON.stringify(Store.getState().dashboard || {})}
-            </pre>
-            <br>
-            <pre>
-                ${JSON.stringify(Store.getState().weight || {})}
-            </pre>`;
-        });
         document.getElementById('load-button').onclick = (ev) => {
             GetDashboard(1).then(data => {
                 Store.dispatch({ type: 'load_dashboard', data });
             });
-        }
-
-        document.getElementById('insert-weight-form').onsubmit = (ev) => {
-            ev.preventDefault();
-            const date = ev.target['insert-weight-form-date'].value;
-            const weight = Number(ev.target['insert-weight-form-weight'].value);
-            InsertWeightEntry(1, { date, weight }).then(data => Store.dispatch({ type: 'weight_inserted', data }));
         }
     }
 }
--- app/renderApplication.jsx
+++ app/renderApplication.jsx
@@ -1,11 +1,24 @@
 import DashbardComponent from './components/dashboardcomponent';
 import React from 'react';
 import { render } from 'react-dom';
+import InsertWeightComponent from './components/insertWeightComponent';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <DashbardComponent />
+                <InsertWeightComponent />
+            </div>
+        );
+    }
+}
+
 const renderApplication = () => {
-    render(<MuiThemeProvider><DashbardComponent /></MuiThemeProvider>, document.getElementById('applicatie'));
+    render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('applicatie'));
 }
 
 export default renderApplication;
